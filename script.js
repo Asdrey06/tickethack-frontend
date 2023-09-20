6dd023e538df26a9ffd6aee4879aca38133b49ca
Alexandre <reineart123@gmail.com>
2023-09-20 11:11:16 +0200

    frontendnew

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
-document.querySelector('departure');
+    document.querySelector('#search').addEventListener('click',
+      function () {
+        console.log('click detected')
+       document.querySelector("#departure").value
+       document.querySelector("arrival").value
+       
+        }
+    );
+
+
+    
+    document.querySelector('#date').addEventListener('click',
+      function () {
+       document.querySelector("#btn-date").value
+      
+        }
+    );
