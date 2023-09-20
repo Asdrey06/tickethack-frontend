190890672b9034692da6833dd12c16c98f9d903c
Audrey <audrey_reynaud@hotmail.fr>
2023-09-20 16:41:16 +0200

    pas de merge

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-    
-    document.querySelector('#search').addEventListener('click',
-      function () {
-       document.getElementById("departure").value
-       document.getElementById("arrival").value
-       document.getElementById("date").value
-               console.log('click detected')
-      }
-    )
+document.querySelector("#search").addEventListener("click", function () {
+  document.getElementById("departure").value;
+  document.getElementById("arrival").value;
+  document.getElementById("date").value;
+  console.log("click detected");
+});
 
-    document.querySelector('#btn').addEventListener('click',
-    function () {
-      const main= document.getElementById("").value
-             console.log('click detected')
-    }
-  )
+document.querySelector("#btn").addEventListener("click", function () {
+  const main = document.getElementById("").value;
+  console.log("click detected");
+});
 
-  document.querySelector('#cart').addEventListener('click',
-  function () {
-    const cart= document.getElementById("").value
-           console.log('click detected')
-  }
-)
-document.querySelector('#booking').addEventListener('click',
-function () {
-}
-)
+
+document.querySelector("#cart").addEventListener("click", function () {
+  const cart = document.getElementById("").value;
+  console.log("click detected");
+});
+document.querySelector("#booking").addEventListener("click", function () {});
